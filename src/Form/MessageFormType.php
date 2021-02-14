a2fb69b9a2c1a954fc4c29f70f3590f4540e3b0e
Nils <nils.richard@free.fr>
2021-02-14 14:42:13 +0100

    Removing captcha

--- src/Form/MessageFormType.php
+++ src/Form/MessageFormType.php
@@ -10,8 +10,6 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-use Captcha\Bundle\CaptchaBundle\Form\Type\CaptchaType;
-use Captcha\Bundle\CaptchaBundle\Validator\Constraints\ValidCaptcha;
 
 
 class MessageFormType extends AbstractType
@@ -23,15 +21,15 @@ class MessageFormType extends AbstractType
             ->add('email', EmailType::class, ['help' => 'Nous ne partagerons jamais votre email.',])
             ->add('content', TextareaType::class,['attr' => ['maxlength' => '2000'],])
             ->add('envoyer', SubmitType::class, ['attr' => ['class' => 'btn btn-primary'],])
-            ->add('captchaCode', CaptchaType::class, [
-                'mapped' => false,
-                'captchaConfig' => 'CaptchaMessage',
-                'constraints' => [
-                    new ValidCaptcha([
-                        'message' => 'Invalid captcha, please try again',
-                    ]),
-                ],
-            ])
+            // ->add('captchaCode', CaptchaType::class, [
+            //     'mapped' => false,
+            //     'captchaConfig' => 'CaptchaMessage',
+            //     'constraints' => [
+            //         new ValidCaptcha([
+            //             'message' => 'Invalid captcha, please try again',
+            //         ]),
+            //     ],
+            // ])
         ;
     }
 
